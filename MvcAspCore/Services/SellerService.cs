a91990fdd7e66d3114d66ead0ef25b59abcb98e4
Ivo Oliveira <soidesigncriacao@gmail.com>
2018-12-14 01:22:57 -0200

    Simple Create Form

--- MvcAspCore/Services/SellerService.cs
+++ MvcAspCore/Services/SellerService.cs
@@ -19,5 +19,11 @@ namespace MvcAspCore.Services
         {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
