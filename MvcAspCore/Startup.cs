3c674ef6a56ea7c7b9ae0830efbd57655f2afd83
Ivo Oliveira <soidesigncriacao@gmail.com>
2018-12-13 22:33:38 -0200

    Seeding Service - Populando serviço

--- MvcAspCore/Startup.cs
+++ MvcAspCore/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using MvcAspCore.Models;
+using MvcAspCore.Data;
 
 namespace MvcAspCore
 {
@@ -39,14 +40,17 @@ namespace MvcAspCore
             services.AddDbContext<MvcAspCoreContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("MvcAspCoreContext"), 
                     builder => builder.MigrationsAssembly("MvcAspCore")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
